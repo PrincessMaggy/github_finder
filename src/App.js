b9193611c8b27a15c5d4ed674d8f77abbaa499b9
PrincessMaggy <edoziemagdalene@gmail.com>
2022-10-02 21:46:23 +0100

    Routes and Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// components
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+    <BrowserRouter>
+        <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className=' container px-3 pb-12 mx-auto'>
+          <Routes>
+            <Route path='/github_finder' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+    
+  )
 }
 
 export default App;
