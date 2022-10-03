26d97639104f1e7d9d54aea1fa9d901d10ade9ff
PrincessMaggy <edoziemagdalene@gmail.com>
2022-10-03 11:29:49 +0100

    userlist component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import UserResult from '../components/users/UserResult'
+
 function Home() {
   return (
-    <div>
-      home
-    </div>
+    <>
+      <UserResult />
+    </>
   )
 }
 
