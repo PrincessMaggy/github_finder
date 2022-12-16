1657ea07433b1b4c7dcef8fe478163608a6e3f39
PrincessMaggy <edoziemagdalene@gmail.com>
2022-12-17 00:07:20 +0100

    AlertContext

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route } from 'react-router-dom';
 // components
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Alert from './components/layout/Alert';
 
 // pages
 import Home from './pages/Home';
@@ -11,14 +12,17 @@ import About from './pages/About';
 import NotFound from './pages/NotFound';
 
 import { GithubProvider } from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
 
 function App() {
   return (
     <GithubProvider>
+      <AlertProvider>
         <BrowserRouter>
             <div className='flex flex-col justify-between h-screen'>
             <Navbar />
             <main className=' container px-3 pb-12 mx-auto'>
+              <Alert/>
               <Routes>
                 <Route path='/github_finder' element={<Home />} />
                 <Route path='/about' element={<About />} />
@@ -29,6 +33,7 @@ function App() {
             <Footer />
           </div>
         </BrowserRouter>
+      </AlertProvider>
     </GithubProvider>
   )
 }
